8a279935c8011ce1d145073d2f884a2970ac0bdc
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2023-05-15 22:46:49 +0600

    Done: 1) Стилизация по данным тестов главного и полной страницы тестов сделан(размеры увеличены) [у них один компонент для главной страницы и для внутренной страницы кажется]
    To Do:
    
     - добавить еще несколько уроков с ютубом текстом фотографиями
    
    Fix:

--- src/pagesInSite/Tests/quizВнутренняячерезкнопки/QuizCard.jsx
+++ src/pagesInSite/Tests/quizВнутренняячерезкнопки/QuizCard.jsx
@@ -13,26 +13,26 @@ const QuizCard = (props) => {
 
                 <div className="course__details">
                     <div className="title_style">
-                        <h6 className="course__title mb-4">{title}</h6>
+                        <p style={{fontSize: '21px', fontWeight: 'bolder', color: 'black'}} className="course__title mb-4">{title}</p>
                     </div>
                     <div>
                         <div className=" d-flex justify-content-between align-items-center">
-                            <p className="lesson d-flex align-items-center gap-1">
+                            <p style={{fontSize: '18px'}}  className="lesson d-flex align-items-center gap-1">
                                 <i class="ri-book-open-line"></i> {lesson} Вопросов
                             </p>
 
-                            <p className="students d-flex align-items-center gap-1">
+                            <p style={{fontSize: '18px'}}  className="students d-flex align-items-center gap-1">
                                 <i class="ri-user-line"></i> {students}K
                             </p>
                         </div>
 
                         <div className=" d-flex justify-content-between align-items-center">
-                            <p className="rating d-flex align-items-center gap-1">
+                            <p style={{fontSize: '16px'}}  className="rating d-flex align-items-center gap-1">
                                 <i class="ri-star-fill"></i> {rating}
                             </p>
 
                             <p className="enroll d-flex align-items-center gap-1">
-                                <Link to={`/quiz/${id}`}> Пройти сейчас</Link>
+                                <Link style={{fontSize: '18px'}}  to={`/quiz/${id}`}> Пройти сейчас</Link>
                             </p>
                         </div>
                     </div>
